fe37747edbb4368412649d6989d8f22ba25e8400
Afzal <fmafzalulbare@gmail.com>
2024-11-14 18:59:26 +0600

    banner section create

---
+++ markup/afzal.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/wp-content/themes/scamwatch/_/css/swiper.min.css">
+    <title>scamwatch</title>
+    <link rel="stylesheet" href="/wp-content/themes/scamwatch/_/css/output.css">
+</head>
+
+<body>
+    <p class="text-center">
+        <============================ Header here=============================>
+    </p>
+
+    <section>
+        <div class="banner--wrapper relative">
+            <div class="swiper bannerSlider">
+                <div class="swiper-wrapper ">
+
+                    <div class="swiper-slide ">
+
+                        <div class="min-h-[70vh] w-full bg-center bg-no-repeat bg-cover flex flex-wrap items-end"
+                            style="background-image: url(https://cdn.pixabay.com/photo/2023/11/05/02/07/ai-generated-8366100_960_720.jpg); ">
+                            <div class="holder  relative z-10 w-full mb-8 md:mb-14">
+                                <div class="max-w-[700px]">
+                                    <h2 class="text-6xl font-normal text-white">Know when to stop and check – stay scam
+                                        safe.</h2>
+                                </div>
+                                <div class="btn mt-7">
+                                    <a href="#">Check for scam warning signs</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="swiper-slide ">
+
+                        <div class="min-h-[70vh] w-full bg-center bg-no-repeat bg-cover flex flex-wrap items-end"
+                            style="background-image: url(https://cdn.pixabay.com/photo/2023/11/05/02/07/ai-generated-8366100_960_720.jpg); ">
+                            <div class="holder  relative z-10 w-full mb-8 md:mb-14">
+                                <div class="max-w-[700px]">
+                                    <h2 class="text-6xl font-normal text-white">Know when to stop and check – stay scam
+                                        safe.</h2>
+                                </div>
+                                <div class="btn mt-7">
+                                    <a href="#">Check for scam warning signs</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div>
+                <div class="swiper-button-next">
+                    <svg width="27" height="8" viewBox="0 0 27 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M26.3536 4.35355C26.5488 4.15829 26.5488 3.84171 26.3536 3.64645L23.1716 0.464466C22.9763 0.269204 22.6597 0.269204 22.4645 0.464466C22.2692 0.659728 22.2692 0.976311 22.4645 1.17157L25.2929 4L22.4645 6.82843C22.2692 7.02369 22.2692 7.34027 22.4645 7.53553C22.6597 7.7308 22.9763 7.7308 23.1716 7.53553L26.3536 4.35355ZM0 4.5H26V3.5H0V4.5Z"
+                            fill="#307FD3"></path>
+                    </svg>
+                </div>
+                <div class="swiper-button-prev">
+                    <svg width="27" height="8" viewBox="0 0 27 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M0.646446 3.64645C0.451183 3.84171 0.451183 4.15829 0.646446 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.73079 7.34027 4.73079 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.97631 4.7308 0.659727 4.53553 0.464465C4.34027 0.269203 4.02369 0.269203 3.82843 0.464465L0.646446 3.64645ZM27 3.5L1 3.5L1 4.5L27 4.5L27 3.5Z"
+                            fill="#307FD3"></path>
+                    </svg>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </section>
+    <!-- ============================ End OF Banner Section ============================ -->
+
+    <section>
+        <div class="holder">
+            
+        </div>
+    </section>
+
+    <p class="text-center">
+        <============================ Footer here=============================>
+    </p>
+    <script src="/wp-content/themes/scamwatch/_/js/jquery.min.js"></script>
+    <script src="/wp-content/themes/scamwatch/_/js/swiper.min.js"></script>
+    <script src="/wp-content/themes/scamwatch/_/js/script.js"></script>
+</body>
+
+</html>
