d60cc4026e5dd62805cb9a936713da66fb608d1b
Afzal <fmafzalulbare@gmail.com>
2024-11-15 15:29:58 +0600

    banner slide added

--- markup/afzal.php
+++ markup/afzal.php
@@ -14,70 +14,71 @@
         <============================ Header here=============================>
     </p>
 
-    <section class="h-[528px] md:h-[750px] overflow-hidden relative">
-        <section class="absolute left-0 top-16 lg:top-24 w-full py-5 z-20">
-            <div class="holder">
-                <div
-                    class="text-white font-BBCReithSans text-xs font-normal uppercase tracking-[0.045rem] leading-[100%]">
-                    <a class="text-white/50 hover:text-orange" href="#">Home</a><span class="text-lightBlue mx-0.5"> /
-                    </span>Events
-                </div>
-            </div>
-        </section>
-
+    <section class="h-[528px] md:h-[700px] overflow-hidden relative flex flex-col justify-end">
         <div class="swiper bannerSlider w-full h-full absolute left-0 top-0">
             <div class="swiper-wrapper">
 
-                <div class="swiper-slide ">
+                <div class="swiper-slide relative">
                     <img class="w-full h-full object-cover absolute left-0 top-0 z-0"
                         src="https://bbcstudioprd.wpenginepowered.com/wp-content/uploads/2024/09/Avengers-Infinity-War-TC1-Stage-1-scaled.jpg"
                         alt="Events">
 
-                    <div class="holder relative pt-0 pb-32 md:py-32">
-                        <div class="w-full ">
-                            <h1
-                                class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px]">
-                                EVENTS </h1>
-
+                    <div class="holder relative h-full flex flex-col justify-end">
+                        <div class=" pb-20 md:pb-32">
+                            <div class="max-w-[750px] ">
+                                <h1
+                                    class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px] ">
+                                    Help to spot and avoid scams
+                                </h1>
+                            </div>
+                            <div class="btn mt-8">
+                                <a href="#">Check for scam warning signs</a>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-
-                <div class="swiper-slide">
+                <div class="swiper-slide relative">
                     <img class="w-full h-full object-cover absolute left-0 top-0 z-0"
-                        src="https://bbcstudioprd.wpenginepowered.com/wp-content/uploads/2024/09/TC1-Founders-Forum-2.jpeg"
+                        src="https://bbcstudioprd.wpenginepowered.com/wp-content/uploads/2024/09/Avengers-Infinity-War-TC1-Stage-1-scaled.jpg"
                         alt="Events">
-                    <div class="holder relative pt-0 pb-32 md:py-32">
-                        <div class="w-full relative z-10 flex flex-wrap items-center justify-between gap-30">
-                            <h1
-                                class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px]">
-                                afzal </h1>
 
+                    <div class="holder relative h-full flex flex-col justify-end">
+                        <div class="pb-20 md:pb-32">
+                            <div class="max-w-[750px] ">
+                                <h1
+                                    class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px] ">
+                                    Know when to stop and check – stay scam safe.
+                                </h1>
+                            </div>
+                            <div class="btn mt-8">
+                                <a href="#">Check for scam warning signs</a>
+                            </div>
                         </div>
                     </div>
                 </div>
+
 
             </div>
 
             <!-- swiper controller -->
-            <div class="swiper-button-next after:content-none w-[53px] h-[53px] z-50 right-8">
-                <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53" fill="none">
+            <div class="swiper-button-next after:content-none w-[70px] h-[70px] z-50 right-8 hidden md:block">
+                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 53 53" fill="none">
                     <path
                         d="M29.3497 26.5013L19.1914 16.3429L20.7549 14.7794L32.4767 26.5013L20.7549 38.2231L19.1914 36.6596L29.3497 26.5013Z"
                         fill="white" />
                 </svg>
             </div>
 
-            <div class="swiper-button-prev after:content-none w-[53px] h-[53px] z-50 left-8">
-                <svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53" fill="none">
+            <div class="swiper-button-prev after:content-none w-[70px] h-[70px] z-50 left-8 hidden md:block">
+                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 53 53" fill="none">
                     <path
                         d="M23.6503 26.4987L33.8086 36.6571L32.2451 38.2206L20.5233 26.4987L32.2451 14.7769L33.8086 16.3404L23.6503 26.4987Z"
                         fill="white" />
                 </svg>
             </div>
 
-            <div class="absolute bottom-8 md:bottom-14 w-full">
+            <div class="absolute bottom-8 md:bottom-14 w-full hidden md:block">
                 <div class="holder flex flex-col gap-4">
                     <div
                         class="bg-white/30 swiper-scrollbar !h-px !opacity-100 !relative !left-0 !w-full [&>.swiper-scrollbar-drag]:bg-white">
@@ -120,6 +121,7 @@
         </div>
     </section>
     <!-- ============================ End OF Tab Link Section ============================ -->
+
     <p class="text-center">
         <============================ Footer here=============================>
     </p>
