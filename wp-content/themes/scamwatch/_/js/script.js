25321383d3c489ccf7aeb444b999420bec72197e
Afzal <fmafzalulbare@gmail.com>
2024-11-15 15:34:58 +0600

    slider funtion added

--- wp-content/themes/scamwatch/_/js/script.js
+++ wp-content/themes/scamwatch/_/js/script.js
@@ -1,6 +1,40 @@
 // conflict bugg solution
 var $ = jQuery.noConflict();
 
+
+$(function () {
+  // video banner slider
+  new Swiper(".bannerSlider", {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+      renderFraction: function (currentClass, totalClass) {
+        return (
+          '<span class="' +
+          currentClass +
+          '"></span>' +
+          "<span>/</span>" +
+          '<span class="' +
+          totalClass +
+          '"></span>'
+        );
+      },
+    },
+  });
+});
+
 // mega menu
 const scamLink = document.getElementById("scam-link");
 const megaMenu = document.getElementById("mega-menu");
@@ -48,38 +82,3 @@ scamLink.parentElement.addEventListener("mouseleave", () => {
   );
 });
 
-
-
-
-$(function () {
-  // video banner slider
-  new Swiper(".bannerSlider", {
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-      renderFraction: function (currentClass, totalClass) {
-        return (
-          '<span class="' +
-          currentClass +
-          '"></span>' +
-          "<span>/</span>" +
-          '<span class="' +
-          totalClass +
-          '"></span>'
-        );
-      },
-    },
-  });
-});
