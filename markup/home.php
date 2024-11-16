e236aed22b841a451b199e8fc6f16f2859014499
Afzal <fmafzalulbare@gmail.com>
2024-11-16 17:38:00 +0600

    news image grid create

--- markup/home.php
+++ markup/home.php
@@ -125,84 +125,6 @@
     </header>
     <!-- end of the header section -->
 
-    <section class="h-[528px] md:h-[700px] overflow-hidden relative flex flex-col justify-end">
-        <div class="swiper bannerSlider w-full h-full absolute left-0 top-0">
-            <div class="swiper-wrapper">
-
-                <div class="swiper-slide relative">
-                    <img class="w-full h-full object-cover absolute left-0 top-0 z-0"
-                        src="https://bbcstudioprd.wpenginepowered.com/wp-content/uploads/2024/09/Avengers-Infinity-War-TC1-Stage-1-scaled.jpg"
-                        alt="Events">
-
-                    <div class="holder relative h-full flex flex-col justify-end">
-                        <div class=" pb-20 md:pb-32">
-                            <div class="max-w-[750px] ">
-                                <h1
-                                    class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px] ">
-                                    Help to spot and avoid scams
-                                </h1>
-                            </div>
-                            <div class="mt-8 flex flex-wrap">
-                                <a class="btn" href="#">Check for scam warning signs</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="swiper-slide relative">
-                    <img class="w-full h-full object-cover absolute left-0 top-0 z-0"
-                        src="https://bbcstudioprd.wpenginepowered.com/wp-content/uploads/2024/09/Avengers-Infinity-War-TC1-Stage-1-scaled.jpg"
-                        alt="Events">
-
-                    <div class="holder relative h-full flex flex-col justify-end">
-                        <div class="pb-20 md:pb-32">
-                            <div class="max-w-[750px] ">
-                                <h1
-                                    class="text-white text-32 md:text-56 font-montserrat font-extrabold uppercase leading-[90%] tracking-[0.56px] ">
-                                    Know when to stop and check – stay scam safe.
-                                </h1>
-                            </div>
-                            <div class="mt-8 flex flex-wrap ">
-                                <a class="btn" href="#">Check for scam warning signs</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
-            <!-- swiper controller -->
-            <div class="swiper-button-next after:content-none w-[70px] h-[70px] z-50 right-8 hidden md:block">
-                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 53 53" fill="none">
-                    <path
-                        d="M29.3497 26.5013L19.1914 16.3429L20.7549 14.7794L32.4767 26.5013L20.7549 38.2231L19.1914 36.6596L29.3497 26.5013Z"
-                        fill="white" />
-                </svg>
-            </div>
-
-            <div class="swiper-button-prev after:content-none w-[70px] h-[70px] z-50 left-8 hidden md:block">
-                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 53 53" fill="none">
-                    <path
-                        d="M23.6503 26.4987L33.8086 36.6571L32.2451 38.2206L20.5233 26.4987L32.2451 14.7769L33.8086 16.3404L23.6503 26.4987Z"
-                        fill="white" />
-                </svg>
-            </div>
-
-            <div class="absolute bottom-8 md:bottom-14 w-full hidden md:block">
-                <div class="holder flex flex-col gap-4">
-                    <div
-                        class="bg-white/30 swiper-scrollbar !h-px !opacity-100 !relative !left-0 !w-full [&>.swiper-scrollbar-drag]:bg-white">
-                    </div>
-                    <div
-                        class="relative swiper-pagination text-white text-sm tracking-[-0.14px] leading-none font-extrabold text-right">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    <!-- ============================ End OF Banner Slider Section ============================ -->
     <section class=" bg-bright-Orange relative md:py-20 py-10">
         <div class="holder">
             <div class="max-w-[550px]">
@@ -249,22 +171,6 @@
         </div>
     </section>
     <!-- ============================ End OF Tab Link Section ============================ -->
-
-    <section class="bg-bright-Orange md:py-20 py-10">
-        <div class="holder">
-            <div class="flex justify-between items-center">
-                <div>
-                    <a href="#" class="text-blue1 underline">Home</a>
-                    <span class="text-blue1"> / </span>
-                    <a href="#" class="text-blue1 underline">About us</a>
-                </div>
-            </div>
-            <div class=" mt-5 md:mt-10">
-                <h1 class=" text-3xl md:text-5xl font-bold text-blue1">News and alerts</h1>
-            </div>
-        </div>
-    </section>
-    <!-- ============================ End OF  Banner Section ============================ -->
 
     <section class=" bg-soft-vanilla block-pad">
         <div class="holder">
@@ -401,7 +307,43 @@
         </div>
     </section>
     <!-- ============================ End Grid image Section ============================ -->
-     
+
+    <section class="bg-blue1 py-8 md:py-50">
+        <div class="holder">
+            <div class="flex lg:items-center justify-between gap-5 flex-col lg:flex-row">
+                <div class="space-y-6 lg:shrink-0">
+                    <h4 class="text-white text-4xl leading-none font-obviously font-normal ">Subscribe for our
+                        newsletter</h4>
+
+                    <p class="text-white  font-light hidden lg:block">Lorem ipsum dolor sit amet consectetur. Quisque
+                        et.</p>
+                </div><!-- Newsletter text -->
+
+                <div class="max-w-[44.375rem] w-full">
+                    <form action="" class="flex items-end gap-5 relative">
+                        <div class="flex-1 border-b border-white">
+                            <input type="email" placeholder="Enter your email address*"
+                                class="text-white md:text-blue-700 bg-transparent md:bg-white w-[calc(100%_-_100px)]  md:w-full py-[15px] px-5 border-none leading-none focus:outline-none placeholder:text-white md:placeholder:text-blue-700 placeholder:text-sm placeholder:opacity-50 placeholder: placeholder:font-normal placeholder:first-letter:capitalize">
+                        </div>
+
+                        <button
+                            class="text-white text-sm font-obviously font-normal uppercase tracking-wider md:min-w-[9.625rem] flex items-center md:border border-white gap-3 md:gap-11 md:px-5 md:py-[14px] absolute right-0 bottom-4 md:static md:hover:text-blue-700 md:hover:bg-white duration-500 z-10 group">
+                            explore <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.5"
+                                    d="M2.66781 7.5C2.5352 7.5 2.40802 7.55268 2.31425 7.64645C2.22048 7.74021 2.16781 7.86739 2.16781 8C2.16781 8.13261 2.22048 8.25978 2.31425 8.35355C2.40802 8.44732 2.5352 8.5 2.66781 8.5L2.66781 7.5ZM2.66781 8.5L13.3345 8.5L13.3345 7.5L2.66781 7.5L2.66781 8.5Z"
+                                    fill="white" class="md:group-hover:fill-blue-700 duration-500"></path>
+                                <path d="M9.33447 12L13.3345 8L9.33447 4" stroke="white" stroke-linecap="round"
+                                    stroke-linejoin="round" class="md:group-hover:stroke-blue-700 duration-500"></path>
+                            </svg> </button>
+                    </form>
+                </div><!-- form wrap -->
+            </div>
+        </div>
+    </section>
+    <!-- ============================ End Grid image Section ============================ -->
+    <!-- end of the footer section -->
+
     <footer class="bg-blue1 py-8 md:py-50 ">
         <div class="holder">
             <div class="md:hidden border-b-2 border-white text-white pb-10 space-y-4">
